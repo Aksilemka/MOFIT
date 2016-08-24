cb5770f59db31bb4ba6b969a8ed51376aed0db50
Aksilemka <Aksilemka@users.noreply.github.com>
2016-08-24 16:47:23 +0200

    Add files via upload

---
+++ lab03_c74.cpp
@@ -0,0 +1,60 @@
+/* Projekt C.7.4 (Bednarek) - Rozwiazywanie metoda roznicowa rownania Schrodingera z potencjałem Yukawy */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+
+
+
+using namespace std;
+
+int main(){
+//dane przypadku <------------------------------------------- tu zmieniamy!
+	int l = 1;				// orbitalna liczba kwantowa
+	float d = 2.0;			// dlugosc ekranowania 
+	int dL = 1;				// delta lambda
+	int N = 10000;			// ilosc wezlow
+	double dro = 0.008;		// dlugosc kroku iteracyjnego
+	
+//zmienne symulacji	
+	double R0, R1, R2;		// funkcja falowa w R_{i-1}, R_{i}, R{i+1}
+	double Ln, Lnp1;		// wartosc wlasna: lambda_{n}, lambda_{n+1}
+	double ro; 				// numer iteracji
+
+//przeliczenie zmiennych
+	int L0 = l+1;
+
+
+//zaczynamy
+	cout<<"--------------------> ZACZYNAMY!"<<endl;
+	
+	ofstream plik1("lab03.out");	// nazwa: lab03_l-d-dL.out
+	if(plik1.is_open()){
+		cout<<"-> plik1 otwarty!"<<endl;
+///////////////////////////////////////////////////////// OBSZAR DZIALANIA
+		
+		
+		for(int i = 0; i < N; ++i){
+            // rownanie roznicowe
+            //R2 = 1./(1+i)*( -R0*(i-1) + i*(2+0.25*dro*dro)*R1 + l*(l+1)/(double)i*R1 - lambda*dro*exp(-ro/d)*R1);
+		
+		
+		
+		
+		}
+		
+		
+///////////////////////////////////////////////////////// OBSZAR DZIALANIA		
+        cout<<"-> plik1 zamkniety!"<<endl;
+        plik1.close();
+	} else{
+		cerr << "Blad otwarcia pliku1";
+	}
+
+
+
+	return 0;
+}
